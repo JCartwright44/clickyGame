f2789cfc524676542ef8a9c1e012b44a270a200a
Jason Cartwright <jason.cartwright44@gmail.com>
2019-05-05 10:04:40 -0700

    Updates

---
+++ precache-manifest.0a3762dc33c14323512eb4a2adf38a83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69db7ffcce78da3301fec2da2d9efd78",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "f529470ea319885fe3cc",
+    "url": "/clickyGame/static/css/main.6b617c14.chunk.css"
+  },
+  {
+    "revision": "85a1d543af124641f09c",
+    "url": "/clickyGame/static/js/2.5cb12968.chunk.js"
+  },
+  {
+    "revision": "f529470ea319885fe3cc",
+    "url": "/clickyGame/static/js/main.49318738.chunk.js"
+  },
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  }
+]);
